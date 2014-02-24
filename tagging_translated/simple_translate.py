fbde35640b1ca99a0272610723ae48ea0174ee6c
Daniel Kaufhold <daniel.kaufhold@bitmazk.com>
2014-02-24 15:23:40 +0100

    Deleted simple_translate.py

--- tagging_translated/simple_translate.py
+++
@@ -1,9 +0,0 @@
-"""Registering translated models for the ``tagging_translated`` app."""
-from simple_translation.translation_pool import translation_pool
-
-from tagging.models import Tag
-
-from .models import TagTitle
-
-
-translation_pool.register_translation(Tag, TagTitle)
